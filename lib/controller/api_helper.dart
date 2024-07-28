a9ad2892416052f14f69b17193b2a235af58d314
Praful Patnecha <prafulpatnecha@gmail.com>
2024-07-28 21:18:59 +0530

    28/7/24

--- lib/controller/api_helper.dart
+++ lib/controller/api_helper.dart
@@ -8,7 +8,7 @@ class ApiHelper
   // String api="";
   Future<Map> apiCalling(String callingSearch)
   async {
-    Uri uri=Uri.parse('https://pixabay.com/api/?key=45126041-73e5750c34eec46c215312624&q=$callingSearch');
+    Uri uri=Uri.parse('https://pixabay.com/api/?key=45158888-a401e90cb8eff0b4fc686572c&q=$callingSearch');
     Response response= await http.get(uri);
     if(response.statusCode==200)
       {
